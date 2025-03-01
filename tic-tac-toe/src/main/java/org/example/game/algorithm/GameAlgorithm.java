b2b725d130dc1dff18c641b4291c859e96bcfa22
surfaceUsed <used.surface@gmail.com>
2025-03-01 11:32:00 +0100

    Deleted project

--- tic-tac-toe/src/main/java/org/example/game/algorithm/GameAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package org.example.game.algorithm;
-
-public interface GameAlgorithm {
-
-    char getGamePiece();
-
-    void gameLogic();
-}
--- tic-tac-toe/src/main/java/org/example/game/player/Player.java
+++
@@ -1,8 +0,0 @@
-package org.example.game.player;
-
-public interface Player {
-
-    char getGamePiece();
-
-    void makePlay();
-}
